ff83b9bcf0895ffb4dedf3fc784aaead194ca57f
Amritraj <amritrjakacabu015@gmail.com>
2018-12-21 18:41:19 -0500

    adding sort algorithms folder and Bubble Sort

---
+++ Algorithms/Sort Algorithms/BubbleSort.js
@@ -0,0 +1,15 @@
+function BubbleSort(arr) {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let swapper = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = swapper;
+      }
+    }
+  }
+  return arr;
+}
+
+let arr = [11, 54, 1, 0, -78, 44, -54, 54, 321];
+console.log(BubbleSort(arr));
