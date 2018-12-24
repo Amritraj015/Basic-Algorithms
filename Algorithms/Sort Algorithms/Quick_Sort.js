345227730f85f35366077bd6b51bb0172951bd93
Amritraj <amritrjakacabu015@gmail.com>
2018-12-24 02:09:47 -0500

    Adding Quick Sort

---
+++ Algorithms/Sort Algorithms/Quick_Sort.js
@@ -0,0 +1,27 @@
+function Pivot(arr, start, end) {
+  let pivot = arr[start];
+
+  while (start < end) {
+    while (start < end && arr[--end] >= pivot);
+    if (start < end) arr[start] = arr[end];
+
+    while (start < end && arr[++start] <= pivot);
+    if (start < end) arr[end] = arr[start];
+  }
+
+  arr[end] = pivot;
+  return end;
+}
+
+function QuickSort(arr, start, end) {
+  if (end - start <= 1) return arr;
+
+  let pivotIndex = Pivot(arr, start, end);
+  QuickSort(arr, start, pivotIndex);
+  QuickSort(arr, pivotIndex + 1, end);
+
+  return arr;
+}
+
+let arr = [10, 1, 9, 2, 8, 3, 7, 4, 6, 5];
+console.log(QuickSort(arr, 0, arr.length));
