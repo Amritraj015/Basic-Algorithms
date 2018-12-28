062e50882ee6ca6867b8ac6dc59e8139c47ca18f
Amritraj <amritrjakacabu015@gmail.com>
2018-12-27 19:34:12 -0500

    Adding Stack Data Structure

---
+++ Data Structures/Stack/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var empl = new EmployeeStack(10);
+            var e1 = new Employee("Employee 1");
+            var e2 = new Employee("Employee 2");
+            var e3 = new Employee("Employee 3");
+            var e4 = new Employee("Employee 4");
+            var e5 = new Employee("Employee 5");
+            var e6 = new Employee("Employee 6");
+
+            empl.Push(e1);
+            empl.Push(e2);
+            empl.Push(e3);
+            empl.Push(e4);
+            empl.Push(e5);
+            empl.Push(e6);
+            empl.PrintStack();
+            Console.WriteLine("The Size of the Stack is: {0}", empl.Size());
+
+            empl.Pop();
+            empl.Pop();
+            empl.PrintStack();
+            Console.WriteLine("The Element at the Top of the Stack is: {0}",empl.Peek());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Data Structures/Stack/Stack.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Stack
+{
+    internal class EmployeeStack
+    {
+        private Employee[] _stackEmployees;
+        private int _top;
+
+        public EmployeeStack(int capacity)
+        {
+            _stackEmployees = new Employee[capacity];
+        }
+
+        public void Push(Employee emp)
+        {
+            if (_top == _stackEmployees.Length)
+            {
+                Employee[] newArray = new Employee[2 * _stackEmployees.Length];
+                Array.Copy(_stackEmployees,newArray,_stackEmployees.Length);
+                _stackEmployees = newArray;
+            }
+
+            _stackEmployees[_top++] = emp;
+        }
+
+        public Employee Pop()
+        {
+            if(IsEmpty())
+                throw new Exception("Stack is Empty. Please Push in Employees First.");
+
+            var emp = _stackEmployees[--_top];
+            _stackEmployees[_top] = null;
+            return emp;
+        }
+
+        public bool IsEmpty()
+        {
+            return _top == 0;
+        }
+
+        public Employee Peek()
+        {
+            if (IsEmpty())
+                throw new Exception("Stack is Empty. Please Push in Employees First.");
+            return _stackEmployees[_top - 1];
+        }
+
+        public int Size()
+        {
+            return _top;
+        }
+
+        public void PrintStack()
+        {
+            for (var i = _top - 1; i >= 0; i--)
+            {
+                Console.WriteLine("{0}", _stackEmployees[i]);
+            }
+        }
+    }
+}
