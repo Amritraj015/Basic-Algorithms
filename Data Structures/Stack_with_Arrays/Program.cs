b2210aa710330ce419b9373f8f789b9f096c7e7e
Amritraj <amritrjakacabu015@gmail.com>
2018-12-27 20:27:46 -0500

    Adding Stack implementation with Linked Lists

---
+++ Data Structures/Stack_with_Arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Stack
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var stack = new EmployeeStack(10);
+            var e1 = new Employee("Employee 1");
+            var e2 = new Employee("Employee 2");
+            var e3 = new Employee("Employee 3");
+            var e4 = new Employee("Employee 4");
+            var e5 = new Employee("Employee 5");
+            var e6 = new Employee("Employee 6");
+
+            stack.Push(e1);
+            stack.Push(e2);
+            stack.Push(e3);
+            stack.Push(e4);
+            stack.Push(e5);
+            stack.Push(e6);
+            stack.PrintStack();
+            Console.WriteLine("The Size of the Stack is: {0}", stack.Size());
+
+            stack.Pop();
+            stack.Pop();
+            stack.PrintStack();
+            Console.WriteLine("The Element at the Top of the Stack is: {0}",stack.Peek());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Data Structures/Stack_with_Linked_Lists/StackLL.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Stack;
+
+namespace Stack_Linked_List
+{
+    class StackLL
+    {
+        private readonly LinkedList<Employee> _stack;
+
+        public StackLL()
+        {
+            _stack = new LinkedList<Employee>();
+        }
+
+        public void Push(Employee emp)
+        {
+            _stack.AddFirst(emp);
+        }
+
+        public void Pop()
+        {
+            _stack.RemoveFirst();
+        }
+
+        public Employee Peek()
+        {
+            return _stack.First.Value;
+        }
+
+        public bool IsEmpty()
+        {
+            return _stack.Count == 0;
+        }
+
+        public void PrintStack()
+        {
+            foreach(var v in _stack)
+            {
+                Console.WriteLine(v.ToString());
+            }
+        }
+    }
+}
