4cc83bd0d3a9e19934836d5fddefcd6768c9e609
Amritraj <amritrjakacabu015@gmail.com>
2018-12-27 22:00:53 -0500

    Adding Queue Data Structure with Arrays

---
+++ Data Structures/Queue_with_Arrays/ArrayQueue.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Stack;
+
+namespace Queue
+{
+    internal class ArrayQueue
+    {
+        private Employee[] _queue;
+        private int _front;
+        private int _back;
+
+        public ArrayQueue(int capacity)
+        {
+            _queue = new Employee[capacity];
+        }
+
+        public void Add(Employee emp)
+        {
+            if (Size() == _queue.Length - 1)
+            {
+                var numItems = Size();
+                var newArray = new Employee[2 * _queue.Length];
+                Array.Copy(_queue,_front,newArray,0,_queue.Length-_front);
+                Array.Copy(_queue,0, newArray,_queue.Length-_front,_back);
+                _queue = newArray;
+
+                _front = 0;
+                _back = numItems;
+            }
+
+            _queue[_back] = emp;
+
+            if (_back < _queue.Length - 1)
+                _back++;
+            else
+                _back = 0;
+        }
+
+        public Employee Remove()
+        {
+            if(Size() == 0)
+                throw new Exception("The Queue is Empty");
+
+            var emp = _queue[_front];
+            _queue[_front++] = null;
+
+            if (Size() == 0)
+            {
+                _front = 0;
+                _back = 0;
+            }
+            else if (_front == _queue.Length)
+                _front = 0;
+
+            return emp;
+        }
+
+        public int Size()
+        {
+            return _front <= _back ? _back - _front : _back - _front + _queue.Length; 
+        }
+
+        public Employee Peek()
+        {
+            if(Size() == 0)
+                throw new Exception("The Queue is Empty");
+
+            return _queue[_front];
+        }
+
+        public void PrintQueue()
+        {
+            if (_front <= _back)
+            {
+                for (var i = _front; i < _back; i++)
+                {
+                    Console.WriteLine(_queue[i].ToString());
+                }
+            }
+            else
+            {
+                for (var i = _front; i < _queue.Length; i++)
+                {
+                    Console.WriteLine(_queue[i].ToString());
+                }
+                for (var i = 0; i < _back; i++)
+                {
+                    Console.WriteLine(_queue[i].ToString());
+                }
+            }
+        }
+    }
+}
---
+++ Data Structures/Queue_with_Arrays/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Stack;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new ArrayQueue(10);
+            var e1 = new Employee("Employee 1");
+            var e2 = new Employee("Employee 2");
+            var e3 = new Employee("Employee 3");
+            var e4 = new Employee("Employee 4");
+            var e5 = new Employee("Employee 5");
+            var e6 = new Employee("Employee 6");
+            var e7 = new Employee("Employee 7");
+            var e8 = new Employee("Employee 8");
+            var e9 = new Employee("Employee 9");
+            var e10 = new Employee("Employee 10");
+            var e11 = new Employee("Employee 11");
+
+
+            queue.Add(e1);
+            queue.Add(e2);
+            queue.Add(e3);
+            queue.Add(e4);
+            queue.Add(e5);
+            queue.Add(e6);
+            queue.Add(e7);
+            queue.Add(e8);
+            queue.Add(e9);
+            queue.Add(e10);
+            queue.Add(e11);
+            queue.Remove();
+            queue.Remove();
+            queue.Remove();
+            queue.Remove();
+            queue.Remove();
+            queue.Remove();
+
+            queue.PrintQueue();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
